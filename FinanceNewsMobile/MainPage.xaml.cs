87b2e86026b2f75aa40e9c45bab029fb56069fc6
Ezel Inan <inanezel@Outlook.com>
2025-05-18 14:40:27 +0000

    UI Update and simple base models created

--- FinanceNewsMobile/MainPage.xaml.cs
+++ FinanceNewsMobile/MainPage.xaml.cs
@@ -3,22 +3,12 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        int? prevRan = null;
+        Random rnd = new Random();
 
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 }
